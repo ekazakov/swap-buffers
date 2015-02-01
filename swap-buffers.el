7fd91e379d9801ef4c5b076cc7f09509da920ddc
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2015-02-01 20:20:47 +0400

    Update instruction

--- swap-buffers.el
+++ swap-buffers.el
@@ -16,10 +16,13 @@
 ;; Install:
 ;;  (require 'swap-buffers)
 ;;
-;; Rebind your C-x o key:
+;; Bind your favorite key to 'swap-buffers:
 ;;  (global-set-key (kbd "C-c b") 'swap-buffers)
 ;;
-
+;; By default 'swap-buffers move focus to second window.
+;; Press C-u C-c b to keep focus in first window
+;;
+;;
 ;;; Code:
 ;;
 ;;; requirements
